365c2c54888555643f8e51af0279f9865754b506
hyuuu <931976205@qq.com>
2019-12-11 21:32:33 +0800

    heycm：弄个测试

--- src/test/java/com/heycm/SpringbootRedisApplicationTests.java
+++ src/test/java/com/heycm/SpringbootRedisApplicationTests.java
@@ -1,13 +1,24 @@
 package com.heycm;
 
+import com.heycm.utils.RedisUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class SpringbootRedisApplicationTests {
 
+    @Autowired
+    RedisUtil redisUtil;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws InterruptedException {
+        redisUtil.set("hhh", "123", 5, TimeUnit.SECONDS);
+        System.out.println(redisUtil.get("hhh"));
+        Thread.sleep(5000);
+        System.out.println(redisUtil.get("hhh"));
     }
 
 }
